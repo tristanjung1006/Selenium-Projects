6fd2a8d2e2cf4ffd6d4be6ec97ab6cb218f5f4bc
SeungWon <jjjssswww1006@gmail.com>
2024-04-21 23:45:33 +0900

    Delete yonsei.py

--- yonsei.py
+++
@@ -1,6 +0,0 @@
-keywords = ["subjtNm", "mlgRank", "mlgReflcResnCtnt"]
-
-with open("sample.txt", "r", encoding="utf-8") as file:
-    for line in file:
-        if any(keyword in line for keyword in keywords):
-            print(line.strip())
